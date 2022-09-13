9c39b5c616dec47350dd7bbdabb8eb7f60131c6f
FranckMagninRobert <franck@linkednode.fr>
2022-09-13 22:39:35 +0200

    fix nav bar js script a refacto vnr

--- ressources/script.js
+++ ressources/script.js
@@ -4,3 +4,28 @@ const navLinks = document.querySelector(".nav_all")
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('light-menu')
 })
+
+const menuAccueil = document.querySelector(".nav-item-link")
+menuAccueil.addEventListener('click',()=>{
+navLinks.classList.toggle('light-menu')
+})
+
+const menuDesc = document.querySelector(".nav-item-desc")
+menuDesc.addEventListener('click',()=>{
+navLinks.classList.toggle('light-menu')
+})
+
+const menuDates = document.querySelector(".nav-item-dates")
+menuDates.addEventListener('click',()=>{
+navLinks.classList.toggle('light-menu')
+})
+
+const menuFaq = document.querySelector(".nav-item-faq")
+menuFaq.addEventListener('click',()=>{
+navLinks.classList.toggle('light-menu')
+})
+
+const menuInsc = document.querySelector(".nav-item-inscriptions")
+menuInsc.addEventListener('click',()=>{
+navLinks.classList.toggle('light-menu')
+})
